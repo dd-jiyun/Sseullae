44e2231db90e50e8602b77d93b198a4296f94aa7
dd-jiyun <dlwldbs8015@gmail.com>
2024-08-06 11:30:13 +0900

    initial settings

---
+++ sseullae_BE/src/main/java/net/sseullae/SseullaeApplication.java
@@ -0,0 +1,13 @@
+package net.sseullae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseullaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseullaeApplication.class, args);
+    }
+
+}
